e97b03afb9759c40b65d0b41ee11676a9c849e6f
Lavanya Ramesh <lavanya.ramesh@cumulocity.com>
2025-02-25 18:07:09 +0530

    fix(c8yscrn): Discard screenshot if there is no difference in the screenshots (#250)
    Previously, when diff is enabled and there is not difference in the screenshots, the file wasn't moved to the output folder which led to images being present under `screenshots.cy.js/..` folder. This update discards the screenshot unless the diff option `diffSkip` is `true`.
    
    ---------
    
    Co-authored-by: Thomas Winkler <thomas.winkler@cumulocity.com>

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -354,12 +354,21 @@ export function configureC8yScreenshotPlugin(
         });
       };
 
+      const deleteFile = (source: string) => {
+        fs.unlink(source, (err) => {
+          if (err) {
+            logScreenshot(`Error deleting file: ${err}`);
+            return reject(err);
+          }
+          logScreenshot(`Deleted ${source}`);
+        });
+      };
       // path contains spec name, remove it. might only be required for run() mode however
       const newPath =
         details.specName.trim() == ""
           ? details.path
           : details.path?.replace(`${details.specName}${path.sep}`, "");
-          logRun(`details.path: ${details.path} -> newPath: ${newPath}`);
+      logRun(`details.path: ${details.path} -> newPath: ${newPath}`);
 
       const screenshotTarget = path.dirname(newPath);
       const diffTarget =
@@ -421,6 +430,8 @@ export function configureC8yScreenshotPlugin(
               logScreenshot(
                 `Skipping ${screenshotFile} (skipMove: ${diffOptions.skipMove})`
               );
+              // discard the screenshot if there is a match and skipMove is false
+              if (!diffOptions.skipMove) deleteFile(details.path);
             } else {
               moveFile(details.path, screenshotFile);
             }
