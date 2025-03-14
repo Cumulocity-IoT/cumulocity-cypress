6d86090a1f507799fa57e6636276294736c9d625
Thomas Winkler <thomas.winkler@cumulocity.com>
2025-03-14 21:25:57 +0100

    fix(c8yscrn): Allow clearing highlights without providing selector (#332)

--- src/c8yscrn/runner.ts
+++ src/c8yscrn/runner.ts
@@ -350,7 +350,18 @@ export class C8yScreenshotRunner {
   protected highlight(action: Action["highlight"], that: C8yScreenshotRunner) {
     const highlights = _.isArray(action) ? action : [action];
     cy.wrap(highlights).each(
-      (highlight: string | SelectableHighlightAction | undefined) => {
+      (
+        highlight:
+          | string
+          | SelectableHighlightAction
+          | { clear: true }
+          | undefined
+      ) => {
+        if (highlight && _.isObject(highlight) && highlight.clear === true) {
+          cy.clearHighlights();
+          return;
+        }
+
         const selector = getSelector(highlight, this.config.selectors);
         if (selector == null) return;
 
@@ -543,7 +554,10 @@ function debug(message: string, options?: any) {
 }
 
 export function isRecording(): boolean {
-  return Cypress.env("C8YCTRL_MODE") === "recording" || Cypress.env("C8YCTRL_MODE") === "record";
+  return (
+    Cypress.env("C8YCTRL_MODE") === "recording" ||
+    Cypress.env("C8YCTRL_MODE") === "record"
+  );
 }
 
 export function isClickAction(action: Action): boolean {
