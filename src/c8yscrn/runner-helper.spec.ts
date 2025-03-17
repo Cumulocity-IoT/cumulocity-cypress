04ee6a816aed1df75a3d1f325a74f88ce38e0fcc
Thomas Winkler <thomas.winkler@cumulocity.com>
2025-03-17 13:05:13 +0100

    Added support for localized selectors with language and localized properties

--- src/c8yscrn/runner-helper.spec.ts
+++ src/c8yscrn/runner-helper.spec.ts
@@ -1,18 +1,14 @@
 /// <reference types="jest" />
 
-import {
-  getSelector,
-  imageName,
-  parseSelector,
-} from "./runner-helper";
+import { getSelector, imageName, parseSelector } from "./runner-helper";
 import { ScreenshotSetup } from "../lib/screenshots/types";
 
 describe("startup", () => {
   describe("getSelector", () => {
     const predefinedSelectors: ScreenshotSetup["selectors"] = [
-      { "testSelector": ".test-class" },
-      { "p1": "predefined 1" },
-      { "p2": "predefined 2" },
+      { testSelector: ".test-class" },
+      { p1: "predefined 1" },
+      { p2: "predefined 2" },
     ];
 
     it("should return the selector string if input is a string", () => {
@@ -44,7 +40,10 @@ describe("startup", () => {
     });
 
     it("should return the selector string if input is a array of strings", () => {
-      const result = getSelector(["my", "test", "selector"], predefinedSelectors);
+      const result = getSelector(
+        ["my", "test", "selector"],
+        predefinedSelectors
+      );
       expect(result).toBe("my test selector");
     });
 
@@ -65,34 +64,87 @@ describe("startup", () => {
 
     it("should return the selector string if predefined is object", () => {
       const result = getSelector("p1 abcd p2", {
-        "p1": "predefined 1",
-        "p2": "predefined 2",
+        p1: "predefined 1",
+        p2: "predefined 2",
       });
       expect(result).toBe("predefined 1 abcd predefined 2");
     });
 
     it("should replace multiple occurences of same predefined selector", () => {
       const result = getSelector("p1 p1", {
-        "p1": "predefined 1",
+        p1: "predefined 1",
       });
       expect(result).toBe("predefined 1 predefined 1");
     });
 
     it("should use the longest key from predefined first", () => {
       const result = getSelector("p1.2 abcd p1    p1.2.3", {
-        "p1": "predefined 1",
+        p1: "predefined 1",
         "p1.2": "predefined 1.2",
         "p1.2.3": "predefined 1.2.3",
       });
-      expect(result).toBe("predefined 1.2 abcd predefined 1    predefined 1.2.3");
+      expect(result).toBe(
+        "predefined 1.2 abcd predefined 1    predefined 1.2.3"
+      );
     });
 
     it("should not remove > selector elements", () => {
       const result = getSelector("p1 > abcd > p2", [
-        { "p1": "predefined 1" },
-        { "p2": "predefined 2" },
+        { p1: "predefined 1" },
+        { p2: "predefined 2" },
       ]);
       expect(result).toBe("predefined 1 > abcd > predefined 2");
+    });
+
+    it("should use language specific selector if language is provided", () => {
+      const selector = {
+        language: "de",
+        selector: "testSelector",
+      };
+      const result = getSelector(selector, predefinedSelectors, "de");
+      expect(result).toBe(".test-class");
+    });
+
+    it("should return undefined if language is not in the language array", () => {
+      const selector = { language: ["de", "en"], selector: "testSelector" };
+      const result = getSelector(selector, predefinedSelectors, "fr");
+      expect(result).toBeUndefined();
+    });
+
+    it("should return selector if object has language and data-cy", () => {
+      const selector = { language: "de", "data-cy": "test-cy" };
+      const result = getSelector(selector, predefinedSelectors, "de");
+      expect(result).toBe("[data-cy=test-cy]");
+    });
+
+    it("should return undefied if object is empty", () => {
+      const selector = {};
+      const result = getSelector(selector, predefinedSelectors, "de");
+      expect(result).toBeUndefined();
+    });
+
+    it("should return undefined if object is empty and language is not provided", () => {
+      const selector = {};
+      const result = getSelector(selector, predefinedSelectors);
+      expect(result).toBeUndefined();
+    });
+
+    it("should return the localized selector if language is provided", () => {
+      const selector = { localized: { de: ".test-class" } };
+      const result = getSelector(selector, predefinedSelectors, "de");
+      expect(result).toBe(".test-class");
+    });
+
+    it("should return the localized selector if language is provided and it is an array", () => {
+      const selector = { localized: { de: ".test-class", en: ".test-class-en" } };
+      const result = getSelector(selector, predefinedSelectors, "en");
+      expect(result).toBe(".test-class-en");
+    });
+
+    it("should return undefined if language is not in the localized object", () => {
+      const selector = { localized: { de: ".test-class" } };
+      const result = getSelector(selector, predefinedSelectors, "fr");
+      expect(result).toBeUndefined();
     });
   });
 
@@ -118,7 +170,9 @@ describe("startup", () => {
     });
 
     it("should not split spaces within []", () => {
-      const result = parseSelector(".navbar-right > [data-cy='my test'] > .btn");
+      const result = parseSelector(
+        ".navbar-right > [data-cy='my test'] > .btn"
+      );
       expect(result).toEqual([".navbar-right", "[data-cy='my test']", ".btn"]);
     });
 
