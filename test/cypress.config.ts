c5828b6d2408151356cf188cb235119e81cfaa27
Thomas Winkler <thomas.winkler@cumulocity.com>
2025-06-24 08:18:14 +0200

    Reverted esbuild preprocessing in cypress.config.ts

--- test/cypress.config.ts
+++ test/cypress.config.ts
@@ -1,30 +1,12 @@
 import { defineConfig } from "cypress";
 import { configureC8yPlugin } from "../src/plugin";
 
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
-
 import * as fs from "fs";
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     setupNodeEvents(on, config) {
-      const bundler = createBundler({
-        plugins: [
-          nodeModulesPolyfillPlugin({
-            modules: {
-              fs: true, // required for quicktype-core
-              // Add other Node.js built-ins used by dependencies if needed
-            },
-          }),
-        ],
-        resolveExtensions: [".ts", ".js", ".mjs", ".cjs", ".json"],
-        sourcemap: "inline",
-      });
-
-      on("file:preprocessor", bundler);
-
       configureC8yPlugin(on, config);
 
       on("after:screenshot", (details) => {
